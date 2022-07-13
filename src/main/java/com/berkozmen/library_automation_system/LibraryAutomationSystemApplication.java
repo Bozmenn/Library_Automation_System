efb5bec4115a6ab1ab80b840a0c6af19f26be7e2
Berk Özmen <99732114+Bozmenn@users.noreply.github.com>
2022-07-13 16:42:22 +0300

    Project created

---
+++ src/main/java/com/berkozmen/library_automation_system/LibraryAutomationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.berkozmen.library_automation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAutomationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryAutomationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/berkozmen/library_automation_system/LibraryAutomationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkozmen.library_automation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAutomationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
